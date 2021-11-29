8a485e3f435dc715043404462ed29d5fab28607c
Fred <fredwil25@gmail.com>
2021-11-29 23:47:02 +0200

    added media types to itunes query

--- controller/itunesRequests.js
+++ controller/itunesRequests.js
@@ -1,22 +1,33 @@
 // handle all requests here
 const fetch = require('node-fetch')
 
-const searchTypes = ['movie', 'podcast', 'music', 'musicVideo', 'audiobook', 'shortFilm', 'tvShow', 'software', 'ebook', 'all']
-
-// limit?
-// media?
 const searchiStore = async (media = ['all'], userInput) => {
-  console.log(media)
   /* 
     handle textInput: Any URL-encoded text string. Note: URL encoding replaces spaces with the plus (+) character
   */
-    let correctedUserInput = userInput.replace(/\s/g, '+')
+  let correctedUserInput = userInput.replace(/\s/g, '+')
+
+  /* 
+    The type of results you want returned, relative to the specified media type. For example: movieArtist for a movie media type search. The default is the track entity associated with the specified media type.
+    mediaTypes: 'movie', 'podcast', 'music', 'musicVideo', 'audiobook', 'shortFilm', 'tvShow', 'software', 'ebook', 'all'
+  */
+//  &mediaType=${media}
+  let mediaString = ''
+  media.map(
+    el => {
+      mediaString += `&entity=${el}`
+    }
+  )
 
 
-  
-  const request = await fetch(`https://itunes.apple.com/search?term=${correctedUserInput}entity=${media}&limit=3`)
-  const json = await request.json()
-  return json
+  try{
+    const request = await fetch(`https://itunes.apple.com/search?term=${correctedUserInput}${mediaString}&limit=3`)
+    console.log(request)
+    const json = await request.json()
+    return json
+  } catch(e){
+    console.error(e)
+  }
   // return await request
 }
 
