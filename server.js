7fffb49fb77f5bdeadd0cc0738620970317b4aa4
Fred <fredwil25@gmail.com>
2022-01-23 22:52:11 +0200

    removed helmet

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const {urlencoded, json } = require('express')
 const dotenv = require('dotenv')
-const helmet = require('helmet')
 
 // initialise
 const server = express()
 server.use(json())
 server.use(urlencoded({extended: false}))
 dotenv.config()
-
-// Helmet
-server.use(helmet())
 
 server.use(
   '/api',
@@ -28,7 +24,7 @@ if(process.env.NODE_ENVIROMENT === 'production'){
 }
 
 server.listen(
-  process.env.PORT,
+  process.env.PORT || 3000,
   () => {
     console.log(`Server listening in port:${process.env.PORT}`)
   }
